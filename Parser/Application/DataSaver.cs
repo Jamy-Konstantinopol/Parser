2ff76ac5240c1d3a2b249d0643f8705e86dc3b41
Jaime.Const <piskakosti@gmail.com>
2024-11-16 14:36:30 +0400

    save csv

--- Parser/Application/DataSaver.cs
+++ Parser/Application/DataSaver.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Reflection;
+using System.Text.Json;
 
 namespace Parser
 {
@@ -19,6 +20,25 @@ namespace Parser
         {
             string json = JsonSerializer.Serialize(data, _options);
             File.WriteAllText(path, json);
+        }
+
+        public void SaveCsv<T>(IEnumerable<T> data, string path)
+        {
+            var properties = typeof(T).GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            if (properties == null || properties.Length == 0)
+                return;
+
+            using (var writer = new StreamWriter(path))
+            {
+                foreach (var item in data)
+                {
+                    var values = properties.Select(p => p.GetValue(item, null)?.ToString() ?? "");
+
+                    if (values != null)
+                        writer.WriteLine(string.Join(",", values));
+                }
+            }
         }
     }
 }
--- Parser/Application/Program.cs
+++ Parser/Application/Program.cs
@@ -6,17 +6,20 @@ string rewardCsvPath = "..\\..\\..\\Data\\items.csv";
 string resultJsonPath = "..\\..\\..\\Data\\result.json";
 string resultCsvPath = "..\\..\\..\\Data\\result.csv";
 
-try
-{
+//try
+//{
     var dataLoader = new DataLoader();
     var tasks = dataLoader.GetFromJson<Parser.Task>(taskJsonPath);
     var taskContents = dataLoader.GetFromJson<TaskContent>(fileJsonPath);
     var rewards = dataLoader.GetFromCsv<Reward>(rewardCsvPath);
     var filteredTaskContents = new TasksFilter().FilterTaskContents(taskContents, tasks);
-    var jsonResult = DataGenerator.CreateRewardDictionary(filteredTaskContents.Keys.ToList(), rewards);
-    new DataSaver().SaveJson(jsonResult, resultJsonPath);
-}
-catch (Exception ex)
-{
-    Console.WriteLine("Что-то пошло не так:\n" + ex.Message);
-}
+    var jsonResult = DataGenerator.CreateRewardDictionary(filteredTaskContents, rewards);
+    var dataSaver = new DataSaver();
+    dataSaver.SaveJson(jsonResult, resultJsonPath);
+
+    dataSaver.SaveCsv(jsonResult, resultCsvPath);
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine("Что-то пошло не так:\n" + ex.Message);
+//}
